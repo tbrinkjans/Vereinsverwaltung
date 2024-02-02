ec3fdebac7fe7f4d1b74401d551a76371464c84b
Lennardt Grommel <lg046303@fh-muenster.de>
2024-02-02 09:59:52 +0100

    Implement initial version of FinanceItem, InventoryItem, Item and Location Class

---
+++ Application/src/application/Location.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application;
+
+public class Location {
+    private String id;
+    private String name;
+    private String description;
+    private int size;
+    private boolean isIndoor;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean getIsIndoor() {
+        return isIndoor;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setIsIndoor(boolean isIndoor) {
+        this.isIndoor = isIndoor;
+    }
+    
+    
+}
