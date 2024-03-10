12bb932339980db34d8b2a7f05b0fffde6abc487
Simon <Simon@10.26.110.146>
2024-03-10 09:56:12 +0100

    fix AuthService

--- Application/src/application/service/AuthService.java
+++ Application/src/application/service/AuthService.java
@@ -8,17 +8,17 @@ import application.model.Member;
 
 public class AuthService {
 
-    private final List<Member> members;
+    private List<Member> members;
     private final DatabaseContext context;
     private final MemberService memberService;
 
     public AuthService(DatabaseContext context) {
         this.context = context;
         memberService = new MemberService(context);
-        members = memberService.getAll();
     }
 
     public Member authMember(String firstName, String lastName) {
+        members = memberService.getAll();
         return members.stream()
             .filter(m -> m.getFirstName().equalsIgnoreCase(firstName) && m.getLastName().equalsIgnoreCase(lastName))
             .findFirst()
