f194d3120c838a4d6b92758b9fb7ff026cde7acf
tbrinkjans <tbri@web.de>
2024-03-14 01:03:33 +0100

    foreign key constraints

--- Application/src/application/database/create.sql
+++ Application/src/application/database/create.sql
@@ -1,104 +1,81 @@
+-- ROLE
 CREATE TABLE 'role' (
     'id' varchar(36),
     'name' text,
     'description' text,
     PRIMARY KEY ('id')
 );
-CREATE TABLE 'permission'(
-    'id' varchar(36),
+CREATE TABLE 'role_permission' (
+    'role_id' varchar(36),
     'permission' text,
-    PRIMARY KEY ('id')
+    PRIMARY KEY ('role_id', 'permission'),
+    FOREIGN KEY ('role_id') REFERENCES 'role'('id') ON DELETE CASCADE
 );
-CREATE TABLE 'permission_role'(
-    'id' varchar(36),
-    'role_id' varchar(36),
-    'permission_id' varchar(36),
-    PRIMARY KEY ('id'),
-    FOREIGN KEY ('role_id') REFERENCES 'role'('id') ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY ('permission_id') REFERENCES 'permission'('id') ON DELETE RESTRICT ON UPDATE CASCADE
-);
-CREATE TABLE 'members'(
-    'id' varchar(36),
-    'last_name' text,
-    'first_name' text,
-    'address' text,
-    PRIMARY KEY ('id')
-);
-CREATE TABLE 'member_role'(
-    'id' varchar(36),
-    'members_id' varchar(36),
-    'role_id' varchar(36),
-    PRIMARY KEY ('id'),
-    FOREIGN KEY ('members_id') REFERENCES 'members'('id') ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY ('role_id') REFERENCES 'role'('id') ON DELETE RESTRICT ON UPDATE CASCADE
-);
-CREATE TABLE 'members_team'(
-    'id' varchar(36),
-    'teams_id' varchar(36),
-    'members_id' varchar(36),
-    PRIMARY KEY ('id'),
-    FOREIGN KEY ('members_id') REFERENCES 'members'('id') ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY ('teams_id') REFERENCES 'teams'('id') ON DELETE RESTRICT ON UPDATE CASCADE
-);
-CREATE TABLE 'teams'(
+
+-- TEAM
+CREATE TABLE 'team' (
     'id' varchar(36),
     'name' text,
     'activity' text,
     PRIMARY KEY ('id')
 );
-CREATE TABLE 'room'(
+
+-- MEMBER
+CREATE TABLE 'member' (
     'id' varchar(36),
-    'size' text,
+    'first_name' text,
+    'last_name' text,
     'address' text,
-    'inside' boolean,
-    'name' text,
-    'description' text,
     PRIMARY KEY ('id')
 );
-CREATE TABLE 'room_plan'(
-    'id' varchar(36),
-    'teams_id' varchar(36),
-    'room_id' varchar(36),
-    PRIMARY KEY ('id'),
-    FOREIGN KEY ('room_id') REFERENCES 'room'('id') ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY ('teams_id') REFERENCES 'teams'('id') ON DELETE RESTRICT ON UPDATE CASCADE
+CREATE TABLE 'member_role' (
+    'member_id' varchar(36),
+    'role_id' varchar(36),
+    PRIMARY KEY ('member_id', 'role_id'),
+    FOREIGN KEY ('member_id') REFERENCES 'member'('id') ON DELETE CASCADE,
+    FOREIGN KEY ('role_id') REFERENCES 'role'('id') ON DELETE RESTRICT
 );
-CREATE TABLE 'inventory'(
+CREATE TABLE 'member_team' (
+    'member_id' varchar(36),
+    'team_id' varchar(36),
+    PRIMARY KEY ('member_id', 'team_id'),
+    FOREIGN KEY ('member_id') REFERENCES 'member'('id') ON DELETE CASCADE,
+    FOREIGN KEY ('team_id') REFERENCES 'team'('id') ON DELETE RESTRICT
+);
+
+-- LOCATION
+CREATE TABLE 'location' (
     'id' varchar(36),
     'name' text,
-    'stock' int,
-    'room_id' varchar(36),
     'description' text,
-    'date' date,
-    PRIMARY KEY ('id'),
-    FOREIGN KEY ('room_id') REFERENCES 'room'('id') ON DELETE RESTRICT ON UPDATE CASCADE
+    'inside' boolean,
+    'size' int,
+    PRIMARY KEY ('id')
 );
-CREATE TABLE 'inventory_list'(
-    'id' varchar(36),
-    'teams_id' varchar(36),
-    'inventory_id' varchar(36),
-    PRIMARY KEY ('id'),
-    FOREIGN KEY ('teams_id') REFERENCES 'teams'('id') ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY ('inventory_id') REFERENCES 'inventory'('id') ON DELETE RESTRICT ON UPDATE CASCADE
-);
-CREATE TABLE 'finances'(
+
+-- INVENTORY_ITEM
+CREATE TABLE 'inventory_item' (
     'id' varchar(36),
     'name' text,
-    'value' int,
-    'type' text,
-    'label' text,
-    'source' text,
-    'inventory_id' varchar(36),
+    'date' date,
+    'activity' text,
+    'location_id' varchar(36),
     PRIMARY KEY ('id'),
-    FOREIGN KEY ('inventory_id') REFERENCES 'inventory'('id') ON DELETE RESTRICT ON UPDATE CASCADE
+    FOREIGN KEY ('location_id') REFERENCES 'location'('id') ON DELETE RESTRICT
 );
-CREATE TABLE 'finance_flow'(
+
+-- FINANCE_ITEM
+CREATE TABLE 'finance_item' (
     'id' varchar(36),
-    'finances_id' varchar(36),
-    'members_id' varchar(36),
-    PRIMARY KEY ('id'),
-    FOREIGN KEY ('finances_id') REFERENCES 'finances'('id') ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY ('members_id') REFERENCES 'members' ('id') ON DELETE RESTRICT ON UPDATE CASCADE
+    'name' text,
+    'date' date,
+    'value' int,
+    'source' text,
+    PRIMARY KEY ('id')
 );
-
-
+CREATE TABLE 'finance_item_label' (
+    'finance_item_id' varchar(36),
+    'label' text,
+    PRIMARY KEY ('finance_item_id', 'label'),
+    FOREIGN KEY ('finance_item_id') REFERENCES 'finance_item'('id') ON DELETE CASCADE
+);
--- Application/src/application/database/dummyData.sql
+++ Application/src/application/database/dummyData.sql
@@ -1,38 +1,35 @@
 INSERT INTO 'role' ('id', 'name', 'description')
-VALUES ('27e2fc49-79fc-4bd0-9835-efb0ea6e0994', 'admin', 'admin'),
-('8d769540-0b4c-4743-881a-0d0f9653e28f', 'admin_member', 'Admin Benutzer');
+VALUES ('27e2fc49-79fc-4bd0-9835-efb0ea6e0994', 'Administrator', 'Lese-/Schreibrechte auf alle Bereiche'),
+       ('8d769540-0b4c-4743-881a-0d0f9653e28f', 'Abteilungsleiter', 'Administrator ohne Schreibrecht auf Rollenbereich');
 
-INSERT INTO 'members' ('id', 'last_name', 'first_name', 'address')
-VALUES ('7b53b480-02ef-4893-a6ac-7211d7c622d1', 'Günter', 'Heinz', 'Essen'),
-('45197916-9a46-426e-bbd1-7a1d5381aaa2', 'Mustermann', 'Max', 'Bottrop'),
-('661655c2-78b6-4366-aa72-64e15365216a', 'Peters', 'Hans', 'Hamburg'),
-('fde9af8f-936a-496f-8b0e-2155279627c3', 'Jansen', 'Elke', 'Kiel'),
-('818ee7d6-279e-4c02-8ba4-601282b0b8fc', 'Bach', 'Hendrik', 'München');
+INSERT INTO 'role_permission' ('role_id', 'permission')
+VALUES ('27e2fc49-79fc-4bd0-9835-efb0ea6e0994', 'READ_MEMBERS'),
+       ('27e2fc49-79fc-4bd0-9835-efb0ea6e0994', 'WRITE_MEMBERS'),
+       ('8d769540-0b4c-4743-881a-0d0f9653e28f', 'READ_MEMBERS'),
+       ('8d769540-0b4c-4743-881a-0d0f9653e28f', 'WRITE_MEMBERS');
 
-INSERT INTO 'teams' ('id', 'name', 'activity')
+INSERT INTO 'team' ('id', 'name', 'activity')
 VALUES ('0e8144d1-10e8-4a33-9a7a-030144d3ca30', 'Dortmund', 'Fußball'),
-('018e1d1c7-fa3b-43c2-9e65-a77aa710592', 'Segelfreunde', 'Segeln'),
-('6aa19a28-3de8-48ac-b06b-2733cdefd9a5', 'Grün Grün Bälle', 'Golf');
+       ('018e1d1c7-fa3b-43c2-9e65-a77aa710592', 'Segelfreunde', 'Segeln'),
+       ('6aa19a28-3de8-48ac-b06b-2733cdefd9a5', 'Grün Grün Bälle', 'Golf');
 
-INSERT INTO 'member_role' ('id', 'members_id', 'role_id')
-VALUES ('1eb464ba-88eb-4a25-aee8-29fd0c2ef208', '7b53b480-02ef-4893-a6ac-7211d7c622d1',  '27e2fc49-79fc-4bd0-9835-efb0ea6e0994'),
-('e605f368-f903-461f-a835-1d9ecb7e270c', '45197916-9a46-426e-bbd1-7a1d5381aaa2', '8d769540-0b4c-4743-881a-0d0f9653e28f'),
-('b302665d-6583-4198-bbcb-22b83f744709', '661655c2-78b6-4366-aa72-64e15365216a', '27e2fc49-79fc-4bd0-9835-efb0ea6e0994'),
-('1bdbd233-3e69-45e1-89fd-27aa739a0c91', 'fde9af8f-936a-496f-8b0e-2155279627c3', '8d769540-0b4c-4743-881a-0d0f9653e28f'),
-('cc01ea01-d36f-4eff-8311-6b6b7866d25a', '818ee7d6-279e-4c02-8ba4-601282b0b8fc', '8d769540-0b4c-4743-881a-0d0f9653e28f');
+INSERT INTO 'member' ('id', 'first_name', 'last_name', 'address')
+VALUES ('7b53b480-02ef-4893-a6ac-7211d7c622d1', 'Heinz', 'Günter', 'Essen'),
+       ('45197916-9a46-426e-bbd1-7a1d5381aaa2', 'Max', 'Mustermann', 'Bottrop'),
+       ('661655c2-78b6-4366-aa72-64e15365216a', 'Hans', 'Peters', 'Hamburg'),
+       ('fde9af8f-936a-496f-8b0e-2155279627c3', 'Elke', 'Jansen', 'Kiel'),
+       ('818ee7d6-279e-4c02-8ba4-601282b0b8fc', 'Hendrik', 'Bach', 'München');
 
-INSERT INTO 'members_team' ('id', 'teams_id', 'members_id')
-VALUES ('f7477fff-d4d1-492f-9f12-a052a55cbba5', '0e8144d1-10e8-4a33-9a7a-030144d3ca30',  '7b53b480-02ef-4893-a6ac-7211d7c622d1'),
-('23594b39-dfd1-4025-b8c6-deaadb4c3931', '0e8144d1-10e8-4a33-9a7a-030144d3ca30', '45197916-9a46-426e-bbd1-7a1d5381aaa2'),
-('e8509f3b-348a-4319-92e0-0b0c2b777441', '018e1d1c7-fa3b-43c2-9e65-a77aa710592', '661655c2-78b6-4366-aa72-64e15365216a'),
-('426f86d4-8029-4b2f-a7de-f9a83f2dda3c', '6aa19a28-3de8-48ac-b06b-2733cdefd9a5', 'fde9af8f-936a-496f-8b0e-2155279627c3'),
-('6b481bf9-2155-4774-8bc4-25f05669b98f', '6aa19a28-3de8-48ac-b06b-2733cdefd9a5', '818ee7d6-279e-4c02-8ba4-601282b0b8fc');
+INSERT INTO 'member_role' ('member_id', 'role_id')
+VALUES ('7b53b480-02ef-4893-a6ac-7211d7c622d1', '27e2fc49-79fc-4bd0-9835-efb0ea6e0994'),
+       ('45197916-9a46-426e-bbd1-7a1d5381aaa2', '8d769540-0b4c-4743-881a-0d0f9653e28f'),
+       ('661655c2-78b6-4366-aa72-64e15365216a', '27e2fc49-79fc-4bd0-9835-efb0ea6e0994'),
+       ('fde9af8f-936a-496f-8b0e-2155279627c3', '27e2fc49-79fc-4bd0-9835-efb0ea6e0994'),
+       ('818ee7d6-279e-4c02-8ba4-601282b0b8fc', '8d769540-0b4c-4743-881a-0d0f9653e28f');
 
-INSERT INTO 'permission' ('id', 'permission')
-VALUES ('d7fbd085-eb53-40c7-9fe0-f7a20ab478dd', 'WRITE_MEMBERS'),
-('56364a82-8c8b-4c91-ada4-71fcd2e060f8' , 'READ_MEMBERS');
-
-INSERT INTO 'permission_role' ('id', 'role_id', 'permission_id')
-VALUES ('4785bdf8-d581-4a81-b52f-911c541b3d58', '27e2fc49-79fc-4bd0-9835-efb0ea6e0994', 'd7fbd085-eb53-40c7-9fe0-f7a20ab478dd'),
-('9abd5715-1555-4de2-92f9-58bd8fc6d70d', '27e2fc49-79fc-4bd0-9835-efb0ea6e0994', '56364a82-8c8b-4c91-ada4-71fcd2e060f8'),
-('e92c7951-3ea6-40c8-b3c2-ada1f71cd75a', '8d769540-0b4c-4743-881a-0d0f9653e28f', '56364a82-8c8b-4c91-ada4-71fcd2e060f8');
+INSERT INTO 'member_team' ('member_id', 'team_id')
+VALUES ('7b53b480-02ef-4893-a6ac-7211d7c622d1', '0e8144d1-10e8-4a33-9a7a-030144d3ca30'),
+       ('45197916-9a46-426e-bbd1-7a1d5381aaa2', '0e8144d1-10e8-4a33-9a7a-030144d3ca30'),
+       ('661655c2-78b6-4366-aa72-64e15365216a', '018e1d1c7-fa3b-43c2-9e65-a77aa710592'),
+       ('fde9af8f-936a-496f-8b0e-2155279627c3', '018e1d1c7-fa3b-43c2-9e65-a77aa710592'),
+       ('fde9af8f-936a-496f-8b0e-2155279627c3', '6aa19a28-3de8-48ac-b06b-2733cdefd9a5');
